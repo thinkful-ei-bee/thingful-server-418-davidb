b667e873f499afa33c1210f110c43990fc4045df
David Bolin <david@bolin.at>
2019-04-16 12:32:57 -0700

    updated middleware

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcryptjs');
 const AuthService = require('../auth/auth-service');
 
 function requireAuth(req, res, next) {
@@ -22,15 +23,20 @@ function requireAuth(req, res, next) {
     tokenUserName
   )
     .then(user => {
-      if (!user || user.password !== tokenPassword) {
+      if (!user) {
         return res.status(401).json({ error: 'Unauthorized request' });
       }
+      return bcrypt.compare(tokenPassword, user.password)
+        .then(passwordsMatch => {
+          if (!passwordsMatch) {
+            return res.status(401).json({ error: 'Unauthorized request'});
+          }
 
-  req.user = user;
-
-  next();
-})
-  .catch(next);
+          req.user = user;
+          next();
+        });
+    })
+    .catch(next);
 }
 
 
