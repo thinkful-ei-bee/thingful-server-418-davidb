7e2ffd71d026d3fbec05fdf08af68fc1fa184c7c
David Bolin <david@bolin.at>
2019-04-16 12:27:17 -0700

    updated tests to use helper function

--- test/protected-endpoints.spec.js
+++ test/protected-endpoints.spec.js
@@ -1,81 +1,83 @@
-const knex = require('knex')
-const app = require('../src/app')
-const helpers = require('./test-helpers')
+'use strict';
+/* global supertest */
+const knex = require('knex');
+const app = require('../src/app');
+const helpers = require('./test-helpers');
 
 describe('Protected endpoints', () => {
-    let db
+  let db;
 
-    const {
+  const {
+    testUsers,
+    testThings,
+    testReviews,
+  } = helpers.makeThingsFixtures();
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    });
+    app.set('db', db);
+  });
+
+  after('disconnect from db', () => db.destroy());
+
+  before('cleanup', () => helpers.cleanTables(db));
+
+  afterEach('cleanup', () => helpers.cleanTables(db));
+
+  beforeEach('insert things', () => {
+    return helpers.seedThingsTables(
+      db,
       testUsers,
       testThings,
-      testReviews,
-    } = helpers.makeThingsFixtures()
+      testReviews
+    );
+  });
 
-    before('make knex instance', () => {
-        db = knex({
-          client: 'pg',
-          connection: process.env.TEST_DB_URL,
-        })
-        app.set('db', db)
-      })
+  const protectEndpoints = [
+    {
+      name: 'GET /api/things/:thing_id',
+      path: '/api/things/1'
+    },
+    {
+      name: 'GET /api/things/:thing_id/reviews',
+      path: '/api/things/1/reviews'
+    },
+  ];
 
-    after('disconnect from db', () => db.destroy())
-
-    before('cleanup', () => helpers.cleanTables(db))
-
-    afterEach('cleanup', () => helpers.cleanTables(db))
-
-    beforeEach('insert things', () => {
-      return helpers.seedThingsTables(
-        db,
-        testUsers,
-        testThings,
-        testReviews
-      )
-    });
-
-    const protectEndpoints = [
-      {
-        name: 'GET /api/things/:thing_id',
-        path: '/api/things/1'
-      },
-      {
-        name: 'GET /api/things/:thing_id/reviews',
-        path: '/api/things/1/reviews'
-      },
-    ]
-
-    protectEndpoints.forEach(endpoint => {
+  protectEndpoints.forEach(endpoint => {
     describe(endpoint.name, () => {
-      it(`responds with 401 'Missing basic token' when no basic token`, () => {
+      it('responds with 401 \'Missing basic token\' when no basic token', () => {
         return supertest(app)
           .get(endpoint.path)
-          .expect(401, { error: `Missing basic token` })
+          .expect(401, { error: 'Missing basic token' });
       });
 
-      it(`respond 401 'Unauthorized request' when no credentials in token`, () => {
+      it('respond 401 \'Unauthorized request\' when no credentials in token', () => {
         const userNoCreds = { user_name: '', password: ''};
         return supertest(app)
           .get(endpoint.path)
           .set('Authorization', helpers.makeAuthHeader(userNoCreds))
-          .expect(401, { error: 'Unauthorized request'})
+          .expect(401, { error: 'Unauthorized request'});
       });
 
-      it(`responds 401 'Unauthorized request' when invalid user`, () => {
-        const userInvalidCreds = { user_name: 'user-not', password: 'existy'}
+      it('responds 401 \'Unauthorized request\' when invalid user', () => {
+        const userInvalidCreds = { user_name: 'user-not', password: 'existy'};
         return supertest(app)
           .get(endpoint.path)
           .set('Authorization', helpers.makeAuthHeader(userInvalidCreds))
-          .expect(401, { error: 'Unauthorized request' })
-      })
+          .expect(401, { error: 'Unauthorized request' });
+      });
 
-      it(`responds 401 'Unauthorized request' when invalid password`, () => {
-        const userInvalidPass = { user_name: testUsers[0].user_name, password: 'wrong'}
+      it('responds 401 \'Unauthorized request\' when invalid password', () => {
+        const userInvalidPass = { user_name: testUsers[0].user_name, password: 'wrong'};
         return supertest(app)
           .get(endpoint.path)
           .set('Authorization', helpers.makeAuthHeader(userInvalidPass))
-          .expect(401, { error: `Unauthorized request` })
-      })
-    })
-  })
+          .expect(401, { error: 'Unauthorized request' });
+      });
+    });
   });
+});
